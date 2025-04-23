6c1a5efc702554bc2adcb03bb5c44696f811b379
hiyashinsu <esutarosa.git@gmail.com>
2025-04-24 01:42:10 +0300

    update: some improves for config file

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use dotenv::dotenv;
+
 #[derive(Debug, Clone)]
 pub struct Config {
     pub database_url: String,
@@ -8,14 +10,19 @@ pub struct Config {
 
 impl Config {
     pub fn init() -> Config {
+        dotenv().ok();
+
         let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env");
         let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set in .env");
-        let jwt_maxage = std::env::var("JWT_MAXAGE").expect("JWT_MAXAGE must be set in .env");
+        let jwt_maxage = std::env::var("JWT_MAXAGE")
+            .expect("JWT_MAXAGE must be set in .env")
+            .parse::<i64>()
+            .expect("JWT_MAXAGE must be a valid integer");
 
         Config {
             database_url,
             jwt_secret,
-            jwt_maxage: jwt_maxage.parse::<i64>().unwrap(),
+            jwt_maxage,
             port: 8080,
         }
     }
