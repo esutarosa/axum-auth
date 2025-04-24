ec9f602f9b5d29bcc18a99c7dc5d9e3662944404
hiyashinsu <esutarosa.git@gmail.com>
2025-04-25 02:09:54 +0300

    feat: add middleware, routers and start server

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,74 @@ mod db;
 mod dtos;
 mod entity;
 mod error;
+mod middleware;
+mod routes;
 mod utils;
 
-fn main() {
-    println!("Hello, world!");
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
+
+use config::Config;
+use db::DBClient;
+use routes::create_router;
+use sqlx::postgres::PgPoolOptions;
+use std::sync::Arc;
+use tower_http::cors::CorsLayer;
+use tracing_subscriber::filter::LevelFilter;
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub env: Config,
+    pub db_client: DBClient,
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .init();
+
+    let config = Config::init();
+
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("Connection to the database is successful.");
+            pool
+        }
+        Err(err) => {
+            println!("Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
+        .allow_credentials(true)
+        .allow_methods([Method::GET, Method::POST, Method::PUT]);
+
+    let db_client = DBClient::new(pool);
+    let app_state = AppState {
+        env: config.clone(),
+        db_client,
+    };
+
+    let app = create_router(Arc::new(app_state.clone())).layer(cors.clone());
+
+    println!(
+        "{}",
+        format!("Server is running on http://localhost:{}", config.port)
+    );
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", &config.port))
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
