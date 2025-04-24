50d3f791c1313528bc92dde7529b32e62385deb1
hiyashinsu <esutarosa.git@gmail.com>
2025-04-25 01:52:32 +0300

    feat: add password utility

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod db;
 mod dtos;
 mod entity;
 mod error;
+mod utils;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/utils/password.rs
@@ -0,0 +1,34 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+
+use crate::error::ErrorMessage;
+
+const MAX_PASSWORD_LENGTH: usize = 64;
+
+pub fn hash(password: impl Into<String>) -> Result<String, ErrorMessage> {
+    let password = password.into();
+
+    if password.is_empty() {
+        return Err(ErrorMessage::EmptyPassword);
+    }
+
+    if password.len() > MAX_PASSWORD_LENGTH {
+        return Err(ErrorMessage::ExceededMaxPasswordLength(MAX_PASSWORD_LENGTH));
+    }
+
+    let argon2 = Argon2::new(
+        argon2::Algorithm::Argon2id,
+        argon2::Version::V0x13,
+        argon2::Params::recommended(),
+    );
+
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = argon2
+        .hash_password(password.as_bytes(), &salt)
+        .map_err(|_| ErrorMessage::HashingError)?
+        .to_string();
+
+    Ok(hashed_password)
+}
